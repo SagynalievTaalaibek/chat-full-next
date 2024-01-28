464af0ce11732202f3d09a64afde3489b0a4f697
SagynalievTaalaibek <sagynalievv.t@gmail.com>
2024-01-28 22:24:14 +0600

    add cors

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import messageRouter from "./routes/message";
 import {messageDb} from "./messageDb";
-
+import cors from 'cors';
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/messages', messageRouter);
 
